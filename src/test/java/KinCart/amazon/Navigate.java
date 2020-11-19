8192366a950151c349c328172567aa15e3ebc74d
uma92 <65304844+uma92@users.noreply.github.com>
2020-11-19 15:31:55 -0600

    First steps of task

---
+++ src/test/java/KinCart/amazon/Navigate.java
@@ -0,0 +1,14 @@
+package KinCart.amazon;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class Navigate {
+    @Test
+    public void firstStep(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver
+    }
+
+}
