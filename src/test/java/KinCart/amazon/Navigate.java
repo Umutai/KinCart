33c02c869f30b5fd8a40fcb0512ece5e111bddd9
uma92 <65304844+uma92@users.noreply.github.com>
2020-11-19 17:31:25 -0600

    Created code with navigation and searches and  validated

--- src/test/java/KinCart/amazon/Navigate.java
+++ src/test/java/KinCart/amazon/Navigate.java
@@ -1,14 +1,28 @@
 package KinCart.amazon;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Navigate {
     @Test
-    public void firstStep(){
+    public void firstStep() throws InterruptedException {
         WebDriverManager.chromedriver().setup();
-        WebDriver
+        WebDriver driver=new ChromeDriver();
+        driver.navigate().to("https://www.amazon.com");
+        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+        searchBox.sendKeys("Alexa");
+        WebElement clickButtton =driver.findElement(By.id("nav-search-submit-text"));
+        clickButtton.click();
+        Thread.sleep(1000);
+        WebElement thirdItem = driver.findElement(By.xpath("//div/h2/a/span[@dir='auto']"));
+        thirdItem.click();
+        Assert.assertTrue(thirdItem.isSelected());
+
     }
 
 }
